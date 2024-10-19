2b8818fb0d718ced14c7299960427ee54d8da981
MCD <debchouneharoun@gmail.com>
2024-10-19 22:25:45 +0100

    remove Element Commit

--- src/main/kotlin/LeetCodeKotlin/removeElement.kt
+++ src/main/kotlin/LeetCodeKotlin/removeElement.kt
@@ -1,9 +1,16 @@
 package LeetCodeKotlin
 
 fun removeElement(nums: IntArray, int: Int): Int {
-    return 1
+    var j = 0
+    for (i in 0  until nums.size) {
+        if (nums[i] != int) {
+            nums[j] = nums[i]
+            j++
+        }
+    }
+    return j
 }
 
 fun main(){
-
+    println(removeElement(intArrayOf(1,1,2,3),3))
 }
