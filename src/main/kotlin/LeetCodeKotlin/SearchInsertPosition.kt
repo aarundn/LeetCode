00fd6d27a33b85cdc552baeb316f0170ef458775
MCD <debchouneharoun@gmail.com>
2024-10-26 23:28:24 +0100

    Find Search Insert Position Commit 2

--- src/main/kotlin/LeetCodeKotlin/SearchInsertPosition.kt
+++ src/main/kotlin/LeetCodeKotlin/SearchInsertPosition.kt
@@ -1,8 +1,26 @@
 package LeetCodeKotlin
 
 fun searchInsert(nums: IntArray, target: Int): Int {
-    return 1
-}
-fun main(){
+    var i = 0
+    if (nums.size <= 2 && target > nums[0]){
+            i = 1
+    } else {
+        for (j in 0 until nums.size - 1) {
 
+            if (target > nums[j] && target < nums[j + 1]) {
+                i = j + 1
+            } else if (target == nums[j]) {
+                i = j
+            } else if (target > nums[j + 1]) {
+                i = nums.size
+            }
+        }
+    }
+
+
+    return i
+}
+
+fun main() {
+    println(searchInsert(intArrayOf(1,3), 4))
 }
